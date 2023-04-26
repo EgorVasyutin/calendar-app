0513cc463c76994080556cd2ba022cfd1e4c20a2
Egor <vaspovas@gmail.com>
2023-04-26 18:45:20 +0300

    test

--- functions/api.js
+++ functions/api.js
@@ -1,47 +1,18 @@
 const express = require('express')
-// const cors = require('cors')
-// const dotenv = require('dotenv')
-// const cookieParser = require('cookie-parser')
-// const errorMiddleware = require('./middlewares/error.middleware');
+const dotenv = require('dotenv')
 const serverless = require('serverless-http')
-//
-// dotenv.config()
-//
-// const userRouter = require('./modules/users/user.routes')
-// const todoRouter = require('./modules/todos/todo.routes')
-//
-// const app = express()
-//
-//
-// app
-//   .use(express.json())
-//   .use(cookieParser())
-//   .use(cors({
-//     credentials: true,
-//     origin: process.env.CLIENT_URL
-//   }))
-//   .use('/api_calendar', userRouter)
-//   .use('/api_calendar/todos', todoRouter)
-//   .use(errorMiddleware)
+
+dotenv.config()
+
+const userRouter = require('./modules/users/user.routes')
+const todoRouter = require('./modules/todos/todo.routes')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return {
-    'path': 'Home',
-    'firstName': 'Egor',
-    'lastName': 'Vas'
 
-  }
-})
-
-app.get('/json', (req, res) => {
-  return {
-    'path': 'json',
-    'author': 'Egor'
-
-  }
-})
+app
+  .use('/api_calendar', userRouter)
+  .use('/api_calendar/todos', todoRouter)
 
 module.exports.handler = serverless(app)
 
--- src/database.sql
+++ src/database.sql
@@ -13,62 +13,65 @@ CREATE TABLE cards (
   "isDone" BOOLEAN,
   "priority" VARCHAR (20),
   "type" VARCHAR (20),
-  "startDate" VARCHAR(255),
+  "startDate" VARCHAR(255) ,
   "endDate" VARCHAR(255),
   userId INTEGER REFERENCES users (id)
 );
---
-
-CREATE TABLE cards (
-
-);
 
 
 --
+
+CREATE TABLE cards ();
+
+
 -- CREATE TABLE user_token (
 --  "userId" INTEGER REFERENCES users (id),
 --  "refreshToken" VARCHAR(255)
 -- );
---
 
 
 ALTER TABLE cards ADD COLUMN "startDate" VARCHAR(255);
 ALTER TABLE cards ADD COLUMN status VARCHAR(255);
 
---
+
 --  ALTER TABLE todo RENAME COLUMN is_done TO isDone;
---
---
---
---
---
+
+
+
 -- ALTER TABLE todo ADD userId INTEGER;
---
+
 -- INSERT INTO todos (title, "isDone", userId) VALUES ('Egor',false, 1);
 -- INSERT INTO users (username, email, password) VALUES ('Egor','egit@mail.ru', 123456);
---
+
 -- ALTER TABLE users
 -- ALTER COLUMN status TYPE VARCHAR(255);
---
+
 -- ALTER TABLE users ADD COLUMN email VARCHAR(255)
---
+
 -- ALTER TABLE todo ADD  INTEGER SET DEFAULT 1;
---
---
+
 -- ALTER TABLE todo DROP COLUMN userId;
---
+
 -- ALTER TABLE todo ALTER COLUMN userId SET DEFAULT 1;
---
+
 -- FOREIGN KEY (userId) REFERENCES users (id)
 
---
 -- FOREIGN KEY (userId) REFERENCES users (id)
 
 -- ALTER TABLE todo DROP COLUMN userId;
+
 -- ALTER TABLE todo ADD COLUMN userId SET DEFAULT 1;
 
--- ALTER TABLE todo ADD COLUMN userId INT NOT NULL
+
+
+
+
+
+
+
+
 --     CONSTRAINT user REFERENCES x (a)   -- if x (a) doens't exist, this will fail!
+ 
 --         ON UPDATE CASCADE ON DELETE CASCADE
 
 -- ALTER TABLE todo ADD FOREIGN KEY (userId) REFERENCES users (id) SET DEFAULT 1
