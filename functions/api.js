f596c04276a4bc698ab0b0b36fe067e37d397f89
Egor <vaspovas@gmail.com>
2023-04-27 14:53:48 +0300

    test

--- functions/api.js
+++ functions/api.js
@@ -6,13 +6,27 @@ dotenv.config()
 
 const userRouter = require('../src/modules/users/user.routes')
 const todoRouter = require('../src/modules/todos/todo.routes')
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const errorMiddleware = require("../src/middlewares/error.middleware");
 
 const app = express()
 
 
+// app
+//   .use('/api_calendar', userRouter)
+//   .use('/api_calendar/todos', todoRouter)
+
 app
+  .use(express.json())
+  .use(cookieParser())
+  .use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+  }))
   .use('/api_calendar', userRouter)
   .use('/api_calendar/todos', todoRouter)
+  .use(errorMiddleware)
 
 module.exports.handler = serverless(app)
 
