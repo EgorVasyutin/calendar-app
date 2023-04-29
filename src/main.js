cd91ccf7015add9549bccdd7a0f7b8846538e54f
Egor <vaspovas@gmail.com>
2023-04-29 19:04:48 +0300

    test

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
-const errorMiddleware = require('./middlewares/error.middleware');ё
+const errorMiddleware = require('./middlewares/error.middleware');
 dotenv.config()
 const userRouter = require('./modules/users/user.routes')
 const todoRouter = require('./modules/todos/todo.routes')
