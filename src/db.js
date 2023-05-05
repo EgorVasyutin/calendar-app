dc390f4846c5500e73cd73049dcb694b5aec75d9
Egor <vaspovas@gmail.com>
2023-05-05 19:53:35 +0300

    test

--- src/db.js
+++ src/db.js
@@ -1,13 +1,13 @@
-// const Pool = require('pg').Pool
-//
-// const pool = new Pool({
-//     user: 'postgres',
-//     password: 'admin',
-//     host: 'localhost',
-//     port: 5432,
-//     database: 'calendar_app'
-// })
-//
-// module.exports = pool
-//
-//
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+    user: 'postgres',
+    password: 'vas45644444AZ',
+    host: 'db.sjsbamimxeyiyehghgoo.supabase.co',
+    port: 5432,
+    database: 'postgres'
+})
+
+module.exports = pool
+
+
