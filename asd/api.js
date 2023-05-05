7b1a7ec4528b358afcccd9dd45c14f5cddb4595e
Egor <vaspovas@gmail.com>
2023-05-05 17:59:48 +0300

    test

--- asd/api.js
+++ asd/api.js
@@ -3,11 +3,12 @@ const serverless = require('serverless-http')
 
 const dotenv = require('dotenv')
 dotenv.config()
-// const userRouter = require('../src/modules/users/user.routes')
+const userRouter = require('../src/modules/users/user.routes')
 // const todoRouter = require('../src/modules/todos/todo.routes')
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const errorMiddleware = require("../src/middlewares/error.middleware");
+const userController = require("../src/modules/users/user.controller");
 
 const app = express()
 
@@ -22,15 +23,7 @@ const router = express.Router()
 router.get('/', (req, res) => {
   res.send("lol send is working")
 })
-// const Router = require('express')
-const userController = require('./user.controller')
-
-// const router = new Router()
-
 router.post('/sign-up', userController.singUp)
-router.post('/sign-in', userController.signIn)
-router.post('/logout', userController.logout)
-router.get('/refresh', userController.refresh)
 
 app.use('/.netlify/functions/api/', router)
   .use(express.json())
