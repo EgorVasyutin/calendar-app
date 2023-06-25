541a2d6a015ed489f7a4f158f5cb231a54729304
Egor <vaspovas@gmail.com>
2023-06-25 23:07:31 +0300

    test

--- src/db.js
+++ src/db.js
@@ -1,23 +1,23 @@
-// const Pool = require('pg').Pool
-//
-// const pool = new Pool({
-//     user: 'Egor',
-//     password: 'vas45644444AZ',
-//     host: 'localhost',
-//     port: 3306,
-//     database: 'cw90932_app'
-// })
-//
-// module.exports = pool
-
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'admin',
+    user: 'Egor',
+    password: 'vas45644444AZ',
     host: 'localhost',
-    port: 5432,
-    database: 'calendar_app'
+    port: 3306,
+    database: 'cw90932_app'
 })
 
 module.exports = pool
+
+// const Pool = require('pg').Pool
+//
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'admin',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'calendar_app'
+// })
+//
+// module.exports = pool
