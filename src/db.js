f7a7a5f64fa0328be9329f744b72a8bc70e9ab10
Egor <vaspovas@gmail.com>
2023-06-30 15:16:46 +0300

    перенес на сервер

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 // const Pool = require('pg').Pool
 //
 // const pool = new Pool({
-//     user: 'Egor',
+//     user: 'cw90932_app',
 //     password: 'vas45644444AZ',
 //     host: 'localhost',
 //     port: 3306,
@@ -10,14 +10,38 @@
 //
 // module.exports = pool
 
+// const Pool = require('pg').Pool
+//
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'admin',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'calendar_app'
+// })
+//
+// module.exports = pool
+
+// const { Client } = require('pg');
+//
+// const client = new Client({
+//     user: 'gen_user',
+//     host: '212.113.122.144',
+//     database: 'default_db',
+//     password: 'vas45644444AZ',
+//     port: 5432,
+// });
+//
+// client.connect();
+
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'admin',
-    host: 'localhost',
+    user: 'gen_user',
+    host: '212.113.122.144',
+    database: 'default_db',
+    password: 'vas45644444AZ',
     port: 5432,
-    database: 'calendar_app'
 })
 
 module.exports = pool
